5f1fdad20a59cecbed4a47373495b8a62a55a5ef
tanukana <bobacrypto2@gmail.com>
2023-12-14 04:23:05 -0800

    Add comments for the process of starting and continuing a direct message conversation between users

--- models/directLog.js
+++ models/directLog.js
@@ -3,9 +3,24 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const DirectLog = new Schema({
-  sender: { type: Schema.Types.ObjectId, ref: "User" },
-  recipient: { type: Schema.Types.ObjectId, ref: "User" },
+  users: [{ type: Schema.Types.ObjectId, ref: "User" }],
   log: [{ type: Schema.Types.ObjectId, ref: "Message" }],
 });
+
+// So any time we start a fresh new DM, we will create a DirectLog schema
+// Both users will be pushed into the users array with their usernames
+// DirectLog object will be pushed into both users history array in
+// their directMessages property.
+
+// Any time either user wants to DM one another the web app will query
+// the correct DirectLog to pull up within the initiating user's
+// directMessages history array
+
+// The query will attempt to match our recipients name with our
+// initiators name to be in the users array
+
+// Once a match has been found we pull logs to display in view.
+// on the Frontend we can have a currentDM state that will set
+// itself to the new DirectLog for POST requests to update message logs.
 
 module.exports = mongoose.model("DirectMessage", DirectMessageSchema);
